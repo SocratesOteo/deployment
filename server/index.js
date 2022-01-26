8c1398eeaba5de55eca8962c96c63dd08bd05414
Paloma Oteo <oteokids@gmail.com>
2022-01-26 15:00:02 -0700

    9

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'../home.html'))
 })
 
-app.use('/css', express.static(path.join(__dirname, 'home.css')))
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../home.css'))
+  })
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
